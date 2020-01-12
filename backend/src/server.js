0c259dab4b13ed7481e3547a37b33a482eb1520d
vtorres96 <victorcdc96@gmail.com>
2020-01-12 12:44:51 -0300

    updated to add cors on server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const routes = require("./routes");
+const path = require("path");
 
 require("dotenv/config");
 
@@ -16,7 +17,9 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 server.listen(3333, () => {
